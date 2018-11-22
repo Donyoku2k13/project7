d8338d677d020b22136b467db3c5fa11a1b73a03
Александр Горячев <@local.itstep>
2018-11-22 21:49:01 +0600

    Добавьте файлы проекта.

---
+++ Project7/Source.cpp
@@ -0,0 +1,170 @@
+#include <stdio.h>
+#include <iostream>
+#include<locale.h>
+#include<math.h>
+
+#define e 2.71
+
+int main()
+{
+	
+	/*setlocale(LC_ALL, "");
+	
+	float a, b, x, x1, x2, d, c;
+	printf("a:  ");
+	scanf_s("%f", &a);
+	printf("b:  ");
+	scanf_s("%f", &b);
+	printf("c:  ");
+	scanf_s("%f", &c);
+
+	d = 4 * a*c - pow(b, 2);
+	x1 = (-b + sqrt(d)) / 2 * a;
+		x2 = (-b - sqrt(d)) / 2 * a;
+	printf("x1:%f\n", x1);
+	printf("x2:%f\n", x2);*/
+	
+	
+	/*float a, b, x;
+	printf("a:  ");
+	scanf_s("%f", &a);
+	printf("b:  ");
+	scanf_s("%f", &b);
+
+	x = (12 - b)*a / 2;
+	printf("x:%f\n", x);*/
+	
+	
+	
+	
+	
+	/*float p, ss, ss2, r;
+	printf("p:  ");
+	scanf_s("%f", &p);
+	printf("ss:  ");
+	scanf_s("%f", &ss);
+
+	ss2 = ss - (ss / 100 * 5);
+	r = p / ss2 * 100;
+
+	printf("r:%f\n", r);*/
+
+
+
+
+	/*float n, h, m;
+	printf("n:  ");
+	scanf_s("%f", &n);
+
+	m = n / (float)60;
+	h= m / (float)60;
+
+	printf("N:%f\n", m);
+	printf("m:%f\n", h);*/
+
+	
+	/*float a, b, c, x, y;
+	printf("x:  ");
+	scanf_s("%f", &x);
+	b = 9.5;
+	a = sqrt(abs(x)) + pow(e, abs(b));
+	c = log10(a) + pow(b, 2);
+
+	y = pow(a, 3) / cos(c);
+	printf("y: %f\n", y);*/
+
+
+
+	/*float a, b, t, x, y;
+	printf("x:  ");
+	scanf_s("%f", &x);
+	t = 2;
+	b = pow(log10(abs(x)), 2);
+	a = t * x + abs(sqrt(b));
+
+
+
+	y = pow(cos(a+pow(b,3)),2);
+	printf("y: %f\n", y);*/
+	
+	
+	/*float a, b, c, x, y;
+	printf("x:  ");
+	scanf_s("%f", &x);
+	c = 9;
+	b = x + pow(c, 2);
+	a = pow(abs(b + c), 1 / 3);
+
+	y = pow(cos(b), 2) + b * pow(cos(pow(a,2)), 4);
+	printf("y: %f\n", y);*/
+
+	
+	/*float a, b, p, x, y;
+	printf("x:  ");
+	scanf_s("%f", &x);
+	p = 1;
+	a = exp(sqrt(abs(x)));
+	b = sin(pow(p, 2)) + pow(x, 3);
+	y = pow(a, 3) / pow(b, 2);
+	printf("y: %f\n", y);*/
+	
+	
+	/*float a, b, x, y;
+	printf("x:  ");
+	scanf_s("%f", &x);
+	a = log(x) / log(e);
+	b = pow(e, 2 * x) + a * x;
+	y = x * pow(a, 3) + pow(b, 2);
+	printf("y: %f\n", y);*/
+
+
+	/*float a, b, c, x, y;
+	printf("x:  ");
+	scanf_s("%f", &x);
+	a = log10(x);
+	b = 0.4;
+	c = pow(a,2)+sqrt(b*x);
+	y = pow(e, 2 * x) + pow(9.7, c);
+	printf("y: %f\n", y);*/
+
+
+	/*float a, b, c;
+	printf("a:  ");
+	scanf_s("%f", &a);
+
+	printf("b:  ");
+	scanf_s("%f", &b);
+	
+	 c =(float) sqrt(a*b);
+
+	printf("c: %f\n", c);*/
+	
+	
+	
+	
+	
+	
+	/*float z, n, m;
+	printf("Z:  ");
+	scanf_s("%f", &z);
+
+	 n =z /  (float)1024;
+	m = n / (float)1024;
+
+	printf("N:%f\n", n);
+	printf("m:%f\n", m);*/
+	
+	
+	
+	
+	
+	/*int l, m;
+	printf("Lenght_cm:  ");
+	scanf_s("%d", &l);
+	m = l / 100;
+	printf("%d\n", m);*/
+
+	system("pause");
+
+
+}
